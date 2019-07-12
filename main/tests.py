406b457dfd655a0e223274383850b0612ded6d8e
Piotr Prusinski <prusinski.pio@gmail.com>
2019-07-12 17:21:59 +0200

    add about us page

--- main/tests.py
+++ main/tests.py
@@ -7,3 +7,9 @@ class TestPage(TestCase):
         self.assertTemplateUsed(response, 'home.html')
         self.assertContains(response, 'BookTime')
 
+    def test_about_us_page_works(self):
+        response = self.client.get(reverse("about_us"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'about_us.html')
+        self.assertContains(response, 'BookTime')
+
