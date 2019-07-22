bccbb17ffd3296a1bb55c869bf9553d61279069d
Piotr Prusinski <prusinski.pio@gmail.com>
2019-07-22 11:22:46 +0200

    add product list view with pagination

--- main/urls.py
+++ main/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("about-us/", TemplateView.as_view(template_name="about_us.html"), name="about_us"),
     path("contact-us/", views.ContactUsView.as_view(), name="contact_us"),
+    path("products/<slug:tag>/", views.ProductListView.as_view(), name="products")
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from django.views.generic.edit import FormView
-from main import forms
+from django.shortcuts import get_object_or_404
+from main import forms, models
 
 
 class ContactUsView(FormView):
@@ -12,3 +14,22 @@ class ContactUsView(FormView):
         form.send_mail()
         return super().form_valid(form)
 
+
+class ProductListView(ListView):
+    template_name = 'product_list.html'
+    paginate_by = 4
+
+    def get_queryset(self):
+        tag = self.kwargs['tag']
+        self.tag = None
+        if tag != "all":
+            self.tag = get_object_or_404(
+                models.ProductTag, slug=tag
+            )
+        if self.tag:
+            products = models.Product.objects.active().filter(
+                tags = self.tag
+            )
+        else:
+            products = models.Product.objects.active()
+        return products.order_by("name")
