7c66c7d5c7ed6126565f5ee20efa14b9e2dc7e7d
Piotr Prusinski <prusinski.pio@gmail.com>
2019-07-31 14:59:30 +0200

    write tests for user signup form and views

--- main/tests/test_forms.py
+++ main/tests/test_forms.py
@@ -24,3 +24,20 @@ class TestForm(TestCase):
         })
         self.assertFalse(form.is_valid())
 
+    def test_valid_signup_form_sends_email(self):
+        form = forms.UserCreationForm(
+            {
+            "email": "user@domain.com",
+            "password1": "genericpassword",
+            "password2": "genericpassword",
+            }
+        )
+        self.assertTrue(form.is_valid())
+
+        with self.assertLogs("main.forms", level="INFO") as cm:
+            form.send_mail()
+        self.assertEqual(
+            mail.outbox[0].subject, "Welcome to BookTime"
+        )
+
+        self.assertGreaterEqual(len(cm.output), 1)
