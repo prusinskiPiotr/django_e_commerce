e2652e9d6c519e8bb644a1c07ed3bf56c6452ac8
Piotr Prusinski <prusinski.pio@gmail.com>
2019-08-03 12:36:45 +0200

    add Basket models

--- main/models.py
+++ main/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import (
     AbstractUser,
     BaseUserManager,
 )
+from django.core.validators import MinValueValidator
 
 
 class ActiveManager(models.Manager):
@@ -111,3 +112,24 @@ class Address(models.Model):
 
     def __str__(self):
         return ", ".join([self.name, self.address1, self.address2, self.zip_code, self.city, self.country])
+
+
+class Basket(models.Model):
+    OPEN = 10
+    SUBMITTED = 20
+    STATUSES = ((OPEN, "Open"), (SUBMITTED, "Submitted"))
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    status = models.IntegerField(choices=STATUSES, default=OPEN)
+
+    def is_empty(self):
+        return self.basketline_set.all().count() == 0
+
+    def count(self):
+        return sum(i.quantity for i in self.basketline_set.all())
+
+
+class BasketLine(models.Model):
+    basket = models.ForeignKey(Basket, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1)])
