ac0bf20fad070e07ba98d917dc2da9e31c45b050
Piotr Prusinski <prusinski.pio@gmail.com>
2019-08-15 09:38:32 +0200

    create address selection views and hook it to templates

--- main/forms.py
+++ main/forms.py
@@ -80,3 +80,13 @@ BasketLineFormSet = inlineformset_factory(
     models.Basket, models.BasketLine, fields=("quantity",), extra=0, 
         widgets={"quantity": widgets.PlusMinusNumberInput()},
 )
+
+class AddressSelectionForm(forms.Form):
+    billing_address = forms.ModelChoiceField(queryset=None)
+    shipping_address = forms.ModelChoiceField(queryset=None)
+
+    def __init__(self, user, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        queryset = models.Address.objects.filter(user=user)
+        self.fields['billing_address'].queryset = queryset
+        self.fields['shipping_address'].queryset = queryset
