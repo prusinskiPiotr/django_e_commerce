15323ad71ab041ad7607e8839b12ee41937dad50
Piotr Prusinski <prusinski.pio@gmail.com>
2019-08-15 09:50:38 +0200

    register Orders to admin site

--- main/admin.py
+++ main/admin.py
@@ -83,6 +83,61 @@ class UserAdmin(DjangoUserAdmin):
     ordering = ("email",)
 
 
+class BasketLineInline(admin.TubularInline):
+    model = models.BasketLine
+    raw_id_fields = ("product",)
+
+
+@admin.register(models.Basket)
+class BasketAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "status", "count")
+    list_editable = ("status",)
+    list_filter = ("status",)
+    inlines = (BasketLineInline,)
+
+
+class OrderLineInline(admin.TabularInline):
+    model = models.OrderLine
+    raw_id_fields = ("product",)
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "status")
+    list_eeditable = ("status",)
+    list_filter = ("status", "shipping_country", "date_added")
+    inline = (OrderLineInline,)
+    fielsets = (
+        (None, {"fields": ("user", "status")}),
+        (
+            "Billing info",
+            {
+                "fields": (
+                    "billing_name",
+                    "billing_address1",
+                    "billing_address2",
+                    "billing_zip_code",
+                    "billing_city",
+                    "billing_country",
+                )
+            },
+        ),
+        (
+            "Shipping info",
+            {
+                "fields": (
+                    "shipping_name",
+                    "shipping_address1",
+                    "shipping_address_2",
+                    "shipping_zip_code",
+                    "shipping_city",
+                    "shipping_country",
+                )
+            }
+        )
+    )
+
+
 admin.site.register(models.ProductTag, ProductTagAdmin)
 admin.site.register(models.Product, ProductAdmin)
 admin.site.register(models.ProductImage, ProductImageAdmin)
